365f8f59eaf1c35275f04d139da1bdd1f171c81f
Praveen kumar <Praveen kumar@DESKTOP-3AFSPBB>
2022-12-03 22:01:20 +0530

    class added

--- TestProject/src/com/praveen/tns/Scaneer.java
+++ TestProject/src/com/praveen/tns/Scaneer.java
@@ -1,6 +1,32 @@
 package com.praveen.tns;
 
+import java.util.Scanner;
+
 public class Scaneer {
+	
+  public static void main(String[] array){
+	  
+	  Scanner sc = new Scanner(System.in);
+	  
+	  System.out.println("enter the employee number");
+	  
+	  int eno = sc.nextInt();
+	  
+	  System.out.println("enter the employee name");
+	  
+	  String ename = sc.next();
+	
+	  System.out.println("enter the employee gender");
+	  
+	  String egender = sc.next();
+	  
+	  System.out.println("enter the employee salary");
+	  
+	  double esalary = sc.nextDouble();
+	  
+	  System.out.println("employee number "+eno+" employee name "+ename+" employee gender "+egender+" employee salary "+esalary);
+	  
+  }
 	
 
 }
